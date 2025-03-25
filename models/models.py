f84db2c5c2edfd9649fdfc3e0ba6e9f25fea2cd7
Gerardo Molina Castilla <gerardomolinacastilla@gmail.com>
2025-03-25 13:11:10 +0100

    fff

---
+++ models/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class Clientes(models.Model):
+    _inherit = 'res.partner'
+    
+    codigo_cliente = fields.Char(string="Código Cliente")
+    codigo_contable = fields.Char(string="Código Contable")
+    provincia = fields.Char(string="Provincia")
+    fax = fields.Char(string="Fax", help="Número de fax del cliente")
+    forma_pago = fields.Many2one('account.payment.term', string="Forma de Pago")
